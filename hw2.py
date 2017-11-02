d765c7afbc08295fea56cefc69294f63be584872
anjululla1 <alulla@uncc.edu>
2017-11-02 12:19:18 -0400

    Add files via upload

---
+++ hw2.py
@@ -0,0 +1,36 @@
+#! /user/bin/env python
+
+class GenBank(object):
+    def genbankParser(self):
+        file = "/Users/anju/Desktop/prog2/hw2_hla.gb.txt"
+        my_file = open(file)
+        data = my_file.readlines()
+        #print (data)
+        return data
+    
+    def Parsedinfo(info, data):
+        #parsed = []
+        string = ''
+        for i in data:
+            string = i.strip()
+            #parsed.append(string)
+
+            if string.startswith('ORGANISM'):
+                print (string)
+            
+            if string.startswith('VERSION'):
+                print (string)
+                
+            if string.startswith('ACCESSION'):
+                print (string)
+        return
+
+obj = GenBank()
+file_output = obj.genbankParser()
+info_out = obj.Parsedinfo(file_output)
+
+print ("Name: Anju Lulla")
+print ("Email: alulla@uncc.edu")
+
+
+
